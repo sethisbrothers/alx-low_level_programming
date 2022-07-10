5be7605247270372522965c7919891a8f6488e11
sethisbrothers <sodjisethkokou@gmail.com>
2022-07-10 12:04:58 -0700

    change

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,41 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- * Return:0
- */
-int main(void)
-{
-int i, y;
-for (i = 0; i < 10; i++)
-{
-  putchar (i + '0');
-}
-for (y = 'a'; y <= 'f'; y++)
-{
-  putchar (y);
-}
 
-putchar('\n');
-return (0);
+
+/**
+
+ * main - Entry point
+
+ * Return: (0) Success
+
+ */
+
+int main(void)
+  
+{
+  
+  int i, y;
+  
+  for (i = 0; i < 10; i++)
+    
+    {
+      
+      putchar (i + '0');
+      
+    }
+  
+  for (y = 'a'; y <= 'f'; y++)
+    
+    {
+      
+      putchar (y);
+      
+    }
+  
+
+  
+  putchar('\n');
+  
+  return (0);
+  
 }
